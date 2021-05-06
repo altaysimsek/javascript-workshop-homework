7a9f042309f00a09db8dc65b4febd382604930c3
Altay Simsek <altaysimsek16@gmail.com>
2021-05-07 01:25:39 +0300

    Add mock users for testing

---
+++ src/models/user/mock-users.ts
@@ -0,0 +1,7 @@
+import { User } from './user'
+
+const altay = new User('1', 'Altay Simsek')
+const furkan = new User('2', 'Furkan Portogal')
+const cagatay = new User('3', 'Ckare')
+
+export { altay, furkan, cagatay }
