98c927c1846a4682b197088e6bcca7a40d28aa52
Altay Simsek <altaysimsek16@gmail.com>
2021-05-07 01:25:12 +0300

    Implement test of Users

--- src/models/user/user.test.ts
+++ src/models/user/user.test.ts
@@ -1,9 +1,27 @@
 /* eslint-disable no-undef */
 import { User } from './user'
+import { Room } from '../room/room'
+import { altay, furkan, cagatay } from './mock-users'
 
-describe('[user.ts]', () => {
-  it('creates instance of user', () => {
-    const user = new User()
-    expect(user instanceof User).toBe(true)
+describe('deneme', () => {
+  it('creates instance of User', () => {
+    expect(cagatay instanceof User).toBe(true)
+  })
+
+  it('checking instances of User[cagatay]', () => {
+    expect(cagatay.id).toEqual('3')
+    expect(cagatay.name).toEqual('Ckare')
+  })
+
+  it('getting all users', () => {
+    expect(User.all()).toEqual([altay, furkan, cagatay])
+  })
+
+  it('getting specific users', () => {
+    expect(User.get('1')).toEqual(altay)
+  })
+
+  it('getting instance of Room', () => {
+    expect(altay.getRoom() instanceof Room).toBe(true)
   })
 })
